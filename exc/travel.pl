2843cacbc9114ed7c29a226ac852d0d19d653c15
Inscriptor <Inscriptor@users.noreply.github.com>
2019-09-16 14:18:57 +0700

    Add program listings

---
+++ exc/travel.pl
@@ -0,0 +1,13 @@
+byCar(auckland,hamilton).
+byCar(hamilton,raglan).
+byCar(valmont,saarbruecken).
+byCar(valmont,metz).
+byTrain(metz,frankfurt).
+byTrain(saarbruecken,frankfurt).
+byTrain(metz,paris).
+byTrain(saarbruecken,paris).
+byPlane(frankfurt,bangkok).
+byPlane(frankfurt,singapore).
+byPlane(paris,losAngeles).
+byPlane(bangkok,auckland).
+byPlane(losAngeles,auckland).
